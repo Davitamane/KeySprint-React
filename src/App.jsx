dcfa64cccb7c9e3573e5348278a094c45d93f338
Davitamane <datokhizanishvilii005@gmail.com>
2025-06-30 14:35:52 +0400

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import './App.css';
+import "./App.css";
+import logo from "./images/Logo.svg";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Header />
+    </>
   );
+}
+function Header() {
+  return (
+    <header className="header">
+      <Logo />
+      <div>
+        <Button>Hello</Button>
+        <Button>test</Button>
+      </div>
+      <div>
+        <Button>About us</Button>
+      </div>
+    </header>
+  );
+}
+function Button({ children }) {
+  return <button className="btn">{children}</button>;
+}
+function Logo() {
+  return <img src={logo} alt="logo" />;
 }
 
 export default App;
