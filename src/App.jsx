9dc429d2656a3080ec5940e781713f79c8c41f85
Davitamane <datokhizanishvilii005@gmail.com>
2025-06-30 17:30:41 +0400

    Finished header

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,46 @@
-import "./App.css";
+import "./styles/App.css";
 import logo from "./images/Logo.svg";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("normal");
   return (
     <>
-      <Header />
+      <Header setMode={setMode} mode={mode} />
+      {mode === "normal" ? <p>NORMAL</p> : <p>SPRINT</p>}
     </>
   );
 }
-function Header() {
+function Header({ mode, setMode }) {
   return (
     <header className="header">
-      <Logo />
-      <div>
-        <Button>Hello</Button>
-        <Button>test</Button>
+      <img src={logo} alt="logo" />
+
+      <div className="mode-container">
+        <Button onClick={() => setMode("normal")} active={mode === "normal"}>
+          Normal mode
+        </Button>
+        <Button onClick={() => setMode("sprint")} active={mode === "sprint"}>
+          Sprint mode
+        </Button>
       </div>
-      <div>
-        <Button>About us</Button>
-      </div>
+      <Button
+        onClick={() =>
+          window.open(
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ"
+          )
+        }
+      >
+        About us
+      </Button>
     </header>
   );
 }
-function Button({ children }) {
-  return <button className="btn">{children}</button>;
+function Button({ children, onClick, active = false }) {
+  return (
+    <button onClick={onClick} className={`btn ${active ? "btn-active" : ""}`}>
+      {children}
+    </button>
+  );
 }
-function Logo() {
-  return <img src={logo} alt="logo" />;
-}
-
 export default App;
